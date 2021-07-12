0a5e98d3e75da146cde1e6c13113884e4691625c
Alferyev Vladimir <vladimir.a.alferyev@gmail.com>
2021-07-12 13:24:21 +0500

    initial 8 card

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // TODO: подставлять номер карты нужно сюда между двойными кавычками, без пробелов
-        String number = "346303158374042";
+        String number = "6011066669815198";
         System.out.println(String.format("Result is %s", isValidCardNumber(number) ? "OK" : "FAIL"));
     }
 
